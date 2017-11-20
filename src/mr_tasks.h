7a0278e7c45f813f3ab5afa6e2e2de2c16310da0
jyotisaini <jyotisaini1990@gmail.com>
2017-11-20 14:38:28 -0500

    added mappers emit and hash implementation

--- src/mr_tasks.h
+++ src/mr_tasks.h
@@ -2,6 +2,12 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <functional>
+#include <mutex>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 /* CS6210_TASK Implement this data structureas per your implementation.
 		You will need this when your worker is running the map task*/
@@ -14,6 +20,12 @@ struct BaseMapperInternal {
 		void emit(const std::string& key, const std::string& val);
 
 		/* NOW you can add below, data members and member functions as per the need of your implementation*/
+
+
+  std::string hashKeys(const std::string& key);
+  std::unordered_set<std::string> tempFiles;
+  int outputNum;
+  std::mutex mutex;
 };
 
 
@@ -22,10 +34,30 @@ inline BaseMapperInternal::BaseMapperInternal() {
 
 }
 
-
 /* CS6210_TASK Implement this function */
 inline void BaseMapperInternal::emit(const std::string& key, const std::string& val) {
-	std::cout << "Dummy emit by BaseMapperInternal: " << key << ", " << val << std::endl;
+	//std::cout << "Dummy emit by BaseMapperInternal: " << key << ", " << val << std::endl;
+//  write lines into intermediate files.
+  std::lock_guard<std::mutex> lock(mutex);
+  std::string file = hashKeys(key);
+  std::ofstream myfile(file, std::ios::app);
+  if (myfile.is_open()) {
+    myfile << key << " " << val << std::endl;
+    myfile.close();
+  } else {
+    std::cerr << "Failed to open file " << file << std::endl;
+    exit(-1);
+  }
+  tempFiles.insert(file);
+}
+
+/* hash key to generate output file */
+inline std::string BaseMapperInternal::hashKeys(const std::string& key) {
+   std::hash<std::string> hashFunction;
+   std::string outputFile ="output/temp" +
+      std::to_string(hashFunction(const_cast<std::string&>(key)) % outputNum) +".txt";
+  return outputFile;
+
 }
 
 
