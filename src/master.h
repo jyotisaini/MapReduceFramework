5f212b30eecc70341843a974f038427fb9373c67
Jyoti Saini <jsaini7@gatech.edu>
2017-12-05 22:16:14 -0500

    async mapper

--- src/master.h
+++ src/master.h
@@ -4,6 +4,7 @@
 #include <grpc/support/log.h>
 #include <unistd.h>
 #include <unordered_set>
+#include <dirent.h>
 #include "file_shard.h"
 #include "mapreduce_spec.h"
 #include "masterworker.grpc.pb.h"
@@ -28,9 +29,10 @@ public:
   bool run();
 
 private:
+
   bool callRunMapperTask();
   bool callRunReducerTask();
-  bool remoteCallMap( const std::string& ip_addr_port, const FileShard& file_shard);
+  bool remoteCallMap( const std::string& ip_addr_port, const FileShard& file_shard, int i);
   bool remoteCallReduce(const std::string& ip_addr_port, const std::string& file_name);
   std::string selectIdleWorker();
 
@@ -41,7 +43,7 @@ private:
   std::unordered_map<std::string, WORKER_STATUS> workerStatus;
 
   // save temp filenames from workers
-  std::unordered_set<std::string> tempFileName;
+  std::vector<std::string> tempFileName;
 
   // master built-in thread pool
   std::unique_ptr<ThreadPool> threadPool;
@@ -77,74 +79,100 @@ inline std::string Master::selectIdleWorker() {
   return "";
 }
 
+typedef struct WorkerReplyData {
+std::string worker;
+WorkerReply reply;
+} WorkerReplyData;
+
 bool Master::callRunMapperTask() {
   count_ = fileShards.size();
-  for (int i = 0; i < fileShards.size(); ++i) {
-    threadPool->AddTask([&, i]() {
-      std::string idleWorker;
-      do {
-        {
-          std::lock_guard<std::mutex> lock(mutex);
-          idleWorker = selectIdleWorker();
-        }
-      } while (idleWorker.empty());
-      // map function ...
-      remoteCallMap(idleWorker, fileShards[i]);
-      notEmpty.notify_one();
-    });
-  }
-  return true;
-}
 
-bool Master::remoteCallMap(const std::string& ip_addr_port, const FileShard& file_shard) {
-  std::unique_ptr<MasterWorker::Stub> stub_ = MasterWorker::NewStub(
-      grpc::CreateChannel(ip_addr_port, grpc::InsecureChannelCredentials()));
+  std::vector<WorkerReplyData> reply(fileShards.size());
+  std::vector<grpc::ClientContext> context(fileShards.size());
+  grpc::CompletionQueue cq;
+  grpc::Status rpcStatus[fileShards.size()];
+  std::vector< MasterQuery> query(fileShards.size());
+  std::vector<bool> statusList(fileShards.size());
+  std::vector<void*> retTagList(fileShards.size());
+
+
+  for (int i = 0; i < fileShards.size(); ++i) {
+  	std::cout << "value of i" << i << std::endl;
+    std::string idleWorker;
+      do {
+          idleWorker = selectIdleWorker();
+          std::cout << "Looking for Idle worker " << std::endl;    
+      }
+      while (idleWorker.empty());
+
+   std::unique_ptr<MasterWorker::Stub> stub_ = MasterWorker::NewStub(
+   grpc::CreateChannel(idleWorker, grpc::InsecureChannelCredentials()));
 
   // 1. set grpc query parameters
-  MasterQuery query;
-  query.set_ismap(true);
-  query.set_userid(mrSpec.userId);
-  query.set_outputnum(mrSpec.outputNums);
+  MasterQuery q;
+ 
+  q.set_ismap(true);
+  q.set_workerid(std::to_string(i));
+  q.set_userid(mrSpec.userId);
+  q.set_outputnum(mrSpec.outputNums);
+  query.push_back(std::move(q));
 
-  for (auto& shardmap : file_shard.shardsMap) {
-    ShardInfo* shard_info = query.add_shard();
+  for (auto& shardmap : fileShards[i].shardsMap) {
+    ShardInfo* shard_info = query[i].add_shard();
     shard_info->set_filename(shardmap.first);
     shard_info->set_offstart(static_cast<int>(shardmap.second.first));
     shard_info->set_offend(static_cast<int>(shardmap.second.second));
   }
 
-  // 2. set async grpc service
-  WorkerReply reply;
-  grpc::ClientContext context;
-  grpc::CompletionQueue cq;
-  grpc::Status status;
+  // club shards
 
   std::unique_ptr<grpc::ClientAsyncResponseReader<WorkerReply>> rpc(
-      stub_->AsyncmapReduce(&context, query, &cq));
+      stub_->AsyncmapReduce(&context[i], query[i], &cq));
+  WorkerReplyData replyData;
+  replyData.worker = idleWorker;
+  reply.push_back(replyData);
 
-  rpc->Finish(&reply, &status, (void*)1);
-  void* got_tag;
-  bool ok = false;
-  GPR_ASSERT(cq.Next(&got_tag, &ok));
-  GPR_ASSERT(got_tag == (void*)1);
-  GPR_ASSERT(ok);
+  rpc->Finish(&reply[i].reply, &rpcStatus[i], (void*)&i);
+ }
 
-  if (!status.ok()) {
-    std::cout << status.error_code() << ": " << status.error_message()
-              << std::endl;
-    return false;
+ for(int i =0; i < fileShards.size(); i++) {
+
+  void* tag;
+  bool status = false;
+  std::cout << "waiting for reply" << std::endl;
+  GPR_ASSERT(cq.Next(&tag, &status));
+  statusList[i] = status;
+  retTagList[i]= tag;
+  GPR_ASSERT(statusList[i]);
+
+  int* shardId = static_cast<int*>(tag);
+  if (!status) {
+    std::cout << "Error. Reassinging  Task " << endl;
+
+    workerStatus[reply[*shardId].worker]= IDLE;
+    //reassignTask(shardId);
   }
+
  // 3. master receive intermediate file names
-  std::cout << "receive temp filenames from " << ip_addr_port << std::endl;
-  workerStatus[ip_addr_port] = COMPLETE;
+ else 
+ { 
+ 	std::cout << "receive temp filenames from " << std::endl;
+    workerStatus[reply[*shardId].worker] = COMPLETE;
   
-  int size = reply.tempfiles_size();  
-  for (int i = 0; i < size; ++i) {
-    tempFileName.insert(reply.tempfiles(i).filename());
-  }
+   std::string dirPath = reply[*shardId].reply.directory(); 
+   DIR *dir;
+   struct dirent *ent;
+   if((dir = opendir(dirPath.c_str()))!= NULL){
+   	 while((ent = readdir(dir))!= NULL)
+   	 	tempFileName.push_back(std::move(ent->d_name));
+   }
+ }
+  // tempfile.sort 
+  std::sort (tempFileName.begin(), tempFileName.end());
 
-  // 4. recover server to available
-  workerStatus[ip_addr_port] = IDLE;
+ // 4. recover server to available
+  workerStatus[reply[i].worker] = IDLE;
+ }
 
   return true;
 }
